9ddde8b60f7f4bef7db9a4eb046f30a6b35edcad
Barry Carr <barry@barrycarr.org>
2015-11-10 21:23:46 +0000

    Initial Commit. Async file copy works!

---
+++ pcp_fsharp/Config.fs
@@ -0,0 +1,8 @@
+﻿namespace Config
+
+open System.Collections.Generic
+
+type Config() =
+    member val SourceFolder = "" with get, set
+    member val DestinationFolder = "" with get, set
+    member val Cameras = new Dictionary<string,string>() with get, set
---
+++ pcp_fsharp/Program.fs
@@ -0,0 +1,47 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+// http://www.codeproject.com/Articles/773451/Fsharp-Asynchronous-Workflows
+
+open Newtonsoft.Json
+open Config
+open System
+open System.IO
+open System.Threading.Tasks
+
+let load filename =
+     JsonConvert.DeserializeObject<Config.Config>(File.ReadAllText(filename))
+
+let filelist dir =
+    Directory.GetFiles(dir)
+
+let copyFiles files dest =
+    let asyncAwaitVoidTask (t: Task) =
+        Async.AwaitIAsyncResult(t) |> Async.Ignore
+
+    let copyFile dest f =
+        async {
+            let d = Path.Combine(dest, Path.GetFileName(f))
+            use reader = File.Open(f, FileMode.Open)
+            use writer = File.Create(d)
+            
+            printfn "Copying: %s -> %s" f d
+            
+            do! reader.CopyToAsync(writer) |> asyncAwaitVoidTask
+            do! writer.FlushAsync() |> asyncAwaitVoidTask
+        } |> Async.StartAsTask
+            
+    Task.WhenAll(files |> Array.map (copyFile dest)) 
+    
+
+[<EntryPoint>]
+let main argv =
+    printfn "%A" argv
+    let config = load "./pcp-config.json"
+    let files = filelist config.SourceFolder
+    let results = copyFiles files config.DestinationFolder
+    results.Wait()
+
+    //Console.ReadLine() |> ignore
+
+    0 // return an integer exit code
